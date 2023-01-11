5e394c36edb51ad1df21208757e43b554195ca55
Pavel <kkmit@mail.ru>
2023-01-11 21:50:45 +0300

    first commit in white project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log(1);
+
+const numberOfFilms = +prompt('Сколько фильмо вы посмотрели?', '');
+
+const personalMovieBD = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a =  prompt('Один из последних просмотренных фильмов?',''),
+      b =  prompt('На сколько оцените его?',''),
+      c =  prompt('Один из последних просмотренных фильмов?',''),
+      d =  prompt('На сколько оцените его?','');
+
+      personalMovieBD.movies[a] = b;
+      personalMovieBD.movies[c] = d;
+
+console.log(personalMovieBD);
+
